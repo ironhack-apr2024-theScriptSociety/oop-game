5d9b37c23cbb909ecad9d94057ed63e9c1dd5200
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-04-19 15:07:16 +0200

    fix: display player and obstacles in the middle

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 
 class Player {
     constructor() {
-        this.positionX = 50;
-        this.positionY = 0;
         this.width = 10;
         this.height = 10;
+        this.positionX = 50 - this.width / 2;
+        this.positionY = 0;
 
         this.playerElm = document.getElementById("player");
-        this.playerElm.style.left = this.positionX + "vw";
-        this.playerElm.style.bottom = this.positionY + "vh";
         this.playerElm.style.width = this.width + "vw";
         this.playerElm.style.height = this.height + "vh";
+        this.playerElm.style.left = this.positionX + "vw";
+        this.playerElm.style.bottom = this.positionY + "vh";
     }
     moveLeft() {
         if(this.positionX > 0){
@@ -30,10 +30,10 @@ class Player {
 
 class Obstacle {
     constructor(){
-        this.positionX = 50;
-        this.positionY = 85;
         this.width = 10;
         this.height = 10;
+        this.positionX = 50 - this.width / 2;
+        this.positionY = 85;
         this.obstacleElm = null;
 
         this.createDomElement();
@@ -44,10 +44,10 @@ class Obstacle {
 
         // step2: add content or modify (ex. innerHTML, innerText, setAttribute...)
         this.obstacleElm.className = "obstacle";
-        this.obstacleElm.style.left = this.positionX + "vw";
-        this.obstacleElm.style.bottom = this.positionY + "vh";
         this.obstacleElm.style.width = this.width + "vw";
         this.obstacleElm.style.height = this.height + "vh";
+        this.obstacleElm.style.left = this.positionX + "vw";
+        this.obstacleElm.style.bottom = this.positionY + "vh";
 
         //step3: append to the dom: `parentElm.appendChild()`
         const parentElm = document.getElementById("board");
